45b3cad902fde99535a4e98a2eafadd3109de0df
Hassan Jalil <hassanjalil65@gmail.com>
2022-04-18 02:43:12 +0100

    Updated vehicle model, added colour, type and images link

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Vehicle extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'registration',
+        'make',
+        'range',
+        'model',
+        'derivative',
+        'price',
+        'mileage',
+        'date_on_forecourt',
+    ];
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(VehicleImages::class);
+    }
+
+    public function colour(): HasOne
+    {
+        return $this->hasOne(VehicleColour::class);
+    }
+
+    public function type(): HasOne
+    {
+        return $this->hasOne(VehicleType::class);
+    }
 }
---
+++ database/migrations/2022_04_18_012720_add_colours_and_types_to_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\VehicleType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColoursAndTypesToVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->foreignIdFor(VehicleType::class, 'type_id');
+            $table->foreignIdFor(VehicleColour::class, 'colour_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('colour_id');
+            $table->dropColumn('type_id');
+        });
+    }
+}
