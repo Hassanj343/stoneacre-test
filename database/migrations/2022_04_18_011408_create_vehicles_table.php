848723add4b5f7ed71689801e8224aa951be71df
Hassan Jalil <hassanjalil65@gmail.com>
2022-04-18 02:26:01 +0100

    Added models & migrations

---
+++ database/migrations/2022_04_18_011408_create_vehicles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('registration');
+            $table->string('make');
+            $table->string('range');
+            $table->string('model');
+            $table->string('derivative');
+            $table->decimal('price');
+            $table->integer('mileage');
+            $table->date('date_on_forecourt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
