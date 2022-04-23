f043d7cdb47c238bdc68de219c3220a2810ac997
Hassan Jalil <hassanjalil65@gmail.com>
2022-04-23 03:59:37 +0100

    Added scopes and ford vehicle export command

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -26,6 +26,14 @@ class Vehicle extends Model
         'is_active'
     ];
 
+    protected $casts = [
+        'price' => 'float'
+    ];
+
+    protected $appends = [
+        'price_ex_vat', 'vat_price'
+    ];
+
     public function images(): HasMany
     {
         return $this->hasMany(VehicleImage::class);
@@ -36,13 +44,54 @@ class Vehicle extends Model
         return $this->hasOne(VehicleColour::class);
     }
 
-    public function type(): HasOne
+    public function type(): BelongsTo
     {
-        return $this->hasOne(VehicleType::class);
+        return $this->belongsTo(VehicleType::class);
     }
 
-    public function make(): HasOne
+    public function make(): BelongsTo
     {
-        return $this->hasOne(VehicleMake::class);
+        return $this->belongsTo(VehicleMake::class);
+    }
+
+    public function getPriceExVatAttribute(): float
+    {
+        return round($this->price / 1.2, 2, PHP_ROUND_HALF_DOWN);
+    }
+
+    public function getVatPriceAttribute(): float
+    {
+        return $this->price - $this->getPriceExVatAttribute();
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('is_active', false);
+    }
+
+    public function scopeByType($query, string $type)
+    {
+        return $query->whereHas('type', function ($q) use ($type) {
+            $q->where('name', $type);
+        });
+    }
+
+    public function scopeByColour($query, string $colour)
+    {
+        return $query->whereHas('colour', function ($q) use ($colour) {
+            $q->where('name', $colour);
+        });
+    }
+
+    public function scopeByMake($query, string $make)
+    {
+        return $query->whereHas('make', function ($q) use ($make) {
+            $q->where('name', $make);
+        });
     }
 }
